a41810a9ce007c11425b59f2479de46fd9eb663e
Hulnis <eric.parker13@gmail.com>
2018-04-14 18:01:00 -0400

    first pass at search error fixing: search

--- assets/js/components/result.jsx
+++ assets/js/components/result.jsx
@@ -5,10 +5,10 @@ export default function Result(props) {
   let result = props.result;
   return (
     <Card>
-      <CardImg top src={ result.poster} />
-      <CardTitle>Result Title: { result.title }</CardTitle>
-      <CardTitle>Year Released: { result.year }</CardTitle>
-      <CardTitle>Result Type: { result.type }</CardTitle>
+      <CardImg top src={ result.Poster} />
+      <CardTitle>Result Title: { result.Title }</CardTitle>
+      <CardTitle>Year Released: { result.Year }</CardTitle>
+      <CardTitle>Result Type: { result.Type }</CardTitle>
     </Card>
   )
 }
