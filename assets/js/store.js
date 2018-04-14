aa74e40d0a53fa8854a3920f6066dcf8beddf125
Hulnis <eric.parker13@gmail.com>
2018-04-14 17:39:45 -0400

    first pass at search error fixing; add nav

--- assets/js/store.js
+++ assets/js/store.js
@@ -1,5 +1,5 @@
-import { createStore, combineReducers } from 'redux';
-import deepFreeze from 'deep-freeze';
+import { createStore, combineReducers } from "redux"
+import deepFreeze from "deep-freeze"
 
 let search_state = {
   status: "waiting",
@@ -9,15 +9,15 @@ let search_state = {
 
 function search(state = search_state, action) {
   switch (action.type) {
-    case 'SEARCH_UPDATE':
+    case "SEARCH_UPDATE":
       return Object.assign({}, state, action.data)
-    case 'SEARCH_START':
+    case "SEARCH_START":
       let newData = {
         status: "searching",
         results: [],
       }
       return Object.assign({}, state, newData)
-    case 'SEARCH_END':
+    case "SEARCH_END":
       let newStatus = {
         status: "waiting",
       }
@@ -30,22 +30,36 @@ function search(state = search_state, action) {
 
 function token(state = null, action) {
   switch (action.type) {
-    case 'SET_TOKEN':
-      return action.token;
+    case "SET_TOKEN":
+      return action.token
     default:
-      return state;
+      return state
+  }
+}
+
+let empty_login = {
+  name: "",
+  pass: "",
+}
+
+function login(state = empty_login, action) {
+  switch (action.type) {
+    case "UPDATE_LOGIN_FORM":
+      return Object.assign({}, state, action.data)
+    default:
+      return state
   }
 }
 
 function root_reducer(state0, action) {
-  console.log("reducer", action);
+  console.log("reducer", action)
   // {posts, users, form} is ES6 shorthand for
   // {posts: posts, users: users, form: form}
-  let reducer = combineReducers({search, token});
-  let state1 = reducer(state0, action);
-  console.log("state1", state1);
-  return deepFreeze(state1);
-};
+  let reducer = combineReducers({login, search, token})
+  let state1 = reducer(state0, action)
+  console.log("state1", state1)
+  return deepFreeze(state1)
+}
 
-let store = createStore(root_reducer);
-export default store;
+let store = createStore(root_reducer)
+export default store
