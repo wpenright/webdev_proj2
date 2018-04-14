7a8560c537316f84960ab3d5987a3d26ccd89641
Hulnis <eric.parker13@gmail.com>
2018-04-14 18:18:22 -0400

    first pass at search error fixing: search layout

--- assets/js/store.js
+++ assets/js/store.js
@@ -79,11 +79,31 @@ function login(state = empty_login, action) {
   }
 }
 
+
+let empty_user_form = {
+  name: "",
+  email: "",
+  password: "",
+}
+
+function user_form(state = empty_user_form, action) {
+  switch (action.type) {
+    case 'USER_UPDATE_FORM':
+      return Object.assign({}, state, action.data)
+    case 'USER_CLEAR_FORM':
+      return Object.assign({}, state, empty_user_form)
+    case 'SET_TOKEN':
+      return Object.assign({}, state, action.token)
+    default:
+      return state
+  }
+}
+
 function root_reducer(state0, action) {
   console.log("reducer", action)
   // {posts, users, form} is ES6 shorthand for
   // {posts: posts, users: users, form: form}
-  let reducer = combineReducers({login, reviews, search, token, users})
+  let reducer = combineReducers({login, reviews, search, token, users, user_form})
   let state1 = reducer(state0, action)
   console.log("state1", state1)
   return deepFreeze(state1)
