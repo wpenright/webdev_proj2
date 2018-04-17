975dd5e64dc80e5c05fadd0f5af1a1edf98ac4e8
Hulnis <eric.parker13@gmail.com>
2018-04-17 18:15:17 -0400

    movie page 16

--- assets/js/store.js
+++ assets/js/store.js
@@ -119,7 +119,7 @@ function root_reducer(state0, action) {
   console.log("reducer", action)
   // {posts, users, form} is ES6 shorthand for
   // {posts: posts, users: users, form: form}
-  let reducer = combineReducers({login, review_form, reviews, search, token, users, user_form})
+  let reducer = combineReducers({login, movies, review_form, reviews, search, token, users, user_form})
   let state1 = reducer(state0, action)
   console.log("state1", state1)
   return deepFreeze(state1)
