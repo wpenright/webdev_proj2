09ba12ed150fa906bdebd762156f944c31efa1cd
Hulnis <eric.parker13@gmail.com>
2018-04-17 18:30:47 -0400

    movie page AHHH

--- assets/js/components/review_list.jsx
+++ assets/js/components/review_list.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReviewListItem from "./review_list_item"
 
 export default function ReviewList(params) {
-  let reviews = params.reviews.map((r) => <ReviewListItem key={r.id} review={r} movieLink={params.movieLink}/>)
+  let reviews = params.reviews.map((r) =>
+    <ReviewListItem key={r.id} review={r} movieLink={params.movieLinks}/>
+  )
   return (
     <div>
       { reviews }
