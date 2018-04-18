957eae55296abe7691434554da5870b4be0fce08
Hulnis <eric.parker13@gmail.com>
2018-04-18 18:00:32 -0400

    help

--- assets/js/components/review_form.jsx
+++ assets/js/components/review_form.jsx
@@ -22,11 +22,12 @@ function ReviewForm(props) {
   }
 
   function submit(ev) {
-    api.submit_review(Object.assign({}, props.form, {
+    const newForm = Object.assign({}, props.form, {
       movie_id: props.movie.api_id,
       user_id: props.user_id,
-    }))
-    console.log(props.form)
+    })
+    api.submit_review(newForm)
+    console.log(newForm)
   }
 
   return (
